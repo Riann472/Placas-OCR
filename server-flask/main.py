9b54bf5738c8180fabcd827246ea803aec4f55e9
Riann <rianncarvalhomota472@gmail.com>
2025-05-31 13:41:13 -0300

    docs: adicionando comentarios

--- server-flask/main.py
+++ server-flask/main.py
@@ -1,3 +1,4 @@
+# Imports
 import cv2 as cv
 import numpy as np
 import requests
@@ -6,6 +7,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from ultralytics import YOLO
 
+# Carrega os modelos YOLO para detecção das placas
 model_placa = YOLO("./yoloModels/placa.pt")
 model_texto = YOLO("./yoloModels/textoplaca.pt")
 
@@ -13,11 +15,13 @@ app = Flask(__name__)
 CORS(app)
 
 
+# Função para encodar a imagem para enviar para o servidor node
 def encode_image(img):
     _, img_encoded = cv.imencode(".jpg", img)
     return img_encoded.tobytes()
 
 
+# Função pra mandar a requisição para o ocr
 def enviar_para_ocr(image_bytes):
     res = requests.post(
         "http://localhost:3001/upload",
@@ -28,33 +32,41 @@ def enviar_para_ocr(image_bytes):
     return res.json(), None
 
 
+# Endpoint upload
 @app.route("/upload", methods=["POST"])
 def upload():
     if "file" not in request.files:
         return jsonify({"error": "Você precisa enviar um arquivo."}), 400
 
+    # Caso tenha um arquivo, joga ele em uma variavel
     file = request.files["file"]
+
     if file.filename == "":
         return jsonify({"error": "Nome de arquivo vazio!"}), 400
 
+    # transforma essa imagem em uint8 para o openCV processar a imagem e armazenar em uma variavel
     imgBuffer = np.frombuffer(file.read(), np.uint8)
     image = cv.imdecode(imgBuffer, cv.IMREAD_COLOR)
 
+    # Desativa o calculo de gradientes do torch e detecta a posição da primeira placa encontrada
     with torch.no_grad():
         result_placa = model_placa(image)
     boxes = result_placa[0].boxes
 
+    # Se nao detectar placa nenhuma, assume que o usuario ja enviou a placa cortada e segue para detecção do texto
     if not boxes or len(boxes) != 1:
         with torch.no_grad():
             result_texto = model_texto(image)
         boxes_texto = result_texto[0].boxes
 
+        # Se não detectar textos, assume que o usuario ja cortou o texto ou que não há placas na imagem, assim enviando a imagem original para o OCR
         if not boxes_texto:
             ocr_result, error = enviar_para_ocr(encode_image(image))
             if error:
                 return jsonify(error), 500
             return jsonify({"message": ocr_result}), 200
 
+        # Caso detecte textos de placa, corta a imagem para esse texto e envia para o ocr
         x1, y1, x2, y2 = map(int, boxes_texto[0].xyxy[0].tolist())
         cropped = image[y1:y2, x1:x2]
         ocr_result, error = enviar_para_ocr(encode_image(cropped))
@@ -62,7 +74,7 @@ def upload():
             return jsonify(error), 500
         return jsonify({"message": ocr_result}), 200
 
-    # Uma placa detectada → recorta e refina
+    # Caso detecte placa, corta o texto a partir da placa cortada e envia pro ocr
     x1, y1, x2, y2 = map(int, boxes[0].xyxy[0].tolist())
     cropped_placa = image[y1:y2, x1:x2]
 
@@ -70,6 +82,7 @@ def upload():
         result_texto_na_placa = model_texto(cropped_placa)
     boxes_texto = result_texto_na_placa[0].boxes
 
+    # Se detectar texto na placa, corta e envia pro ocr
     if boxes_texto:
         x1t, y1t, x2t, y2t = map(int, boxes_texto[0].xyxy[0].tolist())
         cropped_placa = cropped_placa[y1t:y2t, x1t:x2t]
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Imports
 const fs = require('fs')
 const express = require('express')
 const cors = require('cors')
@@ -6,18 +7,23 @@ const sharp = require('sharp')
 const OCR = require('tesseract.js')
 require('dotenv').config()
 
+// Storage do multer
 const storage = require('./storage')
 
+// Inicialização e middlewares
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Criação das pastas
 fs.mkdirSync('./processamento', { recursive: true })
 fs.mkdirSync('./uploads', { recursive: true })
 
 const upload = multer({ storage })
 
+// endpoint upload
 app.post('/upload', upload.single('file'), async (req, res) => {
+    // processa a imagem e armazena numa variavel
     const buffer = await sharp(req.file.path)
         .resize({ width: 600 })
         .grayscale()
@@ -26,6 +32,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         .sharpen()
         .toBuffer();
 
+    // apenas para analise da foto processada, comentar se não for analisar
     sharp(buffer).toFile('./processamento/atual.png', (err, info) => {
         if (err) {
             console.error('Erro ao salvar a imagem:', err);
@@ -33,20 +40,24 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         }
     })
 
+    // cria o worker e seta os paramentros
     const worker = await OCR.createWorker('placa')
     await worker.setParameters({
         tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
         tessedit_pageseg_mode: '6',
         user_defined_dpi: '300'
-    }); 
+    });
 
+    // pega os resultados e finaliza o worker
     const result = await worker.recognize(buffer)
     await worker.terminate()
 
+    // regex para pegar apenas texto de placa
     const textoBruto = result.data.text.replace(/[^A-Z0-9]/gi, '')
     const plateRegex = /\b([A-Z]{3}-?\d{4}|[A-Z]{3}\d[A-Z0-9]\d{2})\b/;
     const match = textoBruto.match(plateRegex);
-    // console.log(match)
+
+    // caso bata com o regex, retorna placa, senao, retorna placa nao reconhecida
     if (match) {
         const placa = match[0];
         console.log("Placa identificada:", placa);
@@ -58,6 +69,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     }
 })
 
+// subindo o server
 app.listen(process.env.PORT, () => {
     console.log(`Rodando na porta ${process.env.PORT}`)
 })
